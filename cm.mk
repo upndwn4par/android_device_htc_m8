63281ca22320f892435da7e66f25b744d66eb4a5
upndwn4par <upndwn4par@gmail.com>
2015-04-14 08:12:56 -0400

    Call vendor make files
    Change-Id: I439a5f7ff286266aa34977c9f74906225b04974f

--- cm.mk
+++ cm.mk
@@ -6,4 +6,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit vendor makefiles
+$(call inherit-product, vendor/upndwn4par/config/common.mk)
+$(call inherit-product, vendor/upndwn4par/config/m8.mk)
+
 PRODUCT_NAME := cm_m8
